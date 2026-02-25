f0beb7ae8ae1d8cec9acdfe0e15ab015603fbdd3
karl2603 <arvindrajkarl@gmail.com>
2026-02-25 22:28:53 +0530

    Logo BG removed

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export const LOGO_URL = "/Assets/Logo_1_Black.jpg.jpeg";
+export const LOGO_URL = "/Assets/Logo_1_Black.jpg-removebg-preview.png";
 
 export const PAST_EVENTS = [
   { id: 1, title: "December Meetup", date: "Dec 2024", img: "/Assets/event-1.jpg" },
